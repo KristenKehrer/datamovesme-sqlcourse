f3fc94cb68f6f0ac1d87e6c5410742af7b80911e
Paul Kehrer <pkehrer@cimpress.com>
2018-12-20 10:30:29 -0500

    make schema generation entirely derive from the csvs to make data changes easier

--- sql/createsql.js
+++ sql/createsql.js
@@ -3,149 +3,100 @@ const _ = require('lodash'),
   path = require('path'),
   fs = require('fs'),
   Bluebird = require('bluebird'),
-  sqlite3 = require('sqlite3').verbose()
+  sqlite = require('sqlite')
 
-const csvs = {
-  'billedservices.csv': [
-    `create table billedservices (
-      Order_date TEXT,
-      service_completed TEXT,
-      Currency TEXT,
-      paid INTEGER,
-      status TEXT,
-      customer_id INTEGER);`,
-    'insert into billedservices values'
-  ],
-  'customer_table.csv': [
-    `create table customer (
-      Customer_id INTEGER,
-      Invoice_Month TEXT,
-      august_commission INTEGER,
-      Email_Domain_Type TEXT,
-      Business_type TEXT,
-      First_Conversion_Date TEXT,
-      Last_Login_date TEXT,
-      Country TEXT,
-      State TEXT,
-      ZipCode INTEGER,
-      Do_Not_Email INTEGER,
-      has_facebook INTEGER,
-      has_instagram INTEGER,
-      has_linkedin INTEGER,
-      has_youtube INTEGER,
-      has_twitter INTEGER,
-      has_pinterest INTEGER,
-      Total_Logins INTEGER,
-      Ever_visiting_Blog INTEGER,
-      Times_Account_Disabled INTEGER,
-      Num_Product_Returns INTEGER,
-      total_sales INTEGER,
-      Multi_user_account TEXT,
-      phone INTEGER
-  );`,
-    'insert into customer values'
-  ],
-  'salescall.csv': [
-    `create table salescall (
-      Customer_id INTEGER,
-      Call_Opened TEXT,
-      Call_Closed TEXT,
-      Case_Number INTEGER,
-      Resolution TEXT,
-      Call_Type TEXT,
-      Call_Type_Sub_Category TEXT
-    );`,
-    'insert into salescall values'],
-  'webscrapingdata.csv': [
-    `create table webscrapingdata (
-      customer_id INTEGER,
-      core_scan_errir TEXT,
-      is_published INTEGER,
-      is_service_business INTEGER,
-      word_count INTEGER,
-      form_count INTEGER,
-      image_count INTEGER,
-      has_page_title INTEGER,
-      has_meta_desc INTEGER,
-      has_cart INTEGER,
-      has_email_capture INTEGER,
-      uses_wp INTEGER,
-      uses_weebly INTEGER,
-      uses_ga INTEGER,
-      language TEXT,
-      page_load_time REAL
-    );`,
-    'insert into webscrapingdata values'
-  ]
+const isNotANumber = (val) => _.isNaN(_.toNumber(val))
+const loadCsv = name => csv().fromFile(path.join(__dirname, name))
+
+async function run() {
+  const csvs = getFiles()
+  const db = await createDb()
+  await populateDatabase(db, csvs)
+  db.close()
+  console.log('Complete!')
 }
-const dbPath = path.join(__dirname, 'sqlcourse.db')
-if (fs.existsSync(dbPath)) {
-  fs.unlinkSync(dbPath)
+
+async function createDb() {
+  const dbPath = path.join(__dirname, 'sqlcourse.db')
+  if (fs.existsSync(dbPath)) {
+    fs.unlinkSync(dbPath)
+  }
+  const db = await sqlite.open(dbPath, { promise: Bluebird })
+  db.on('error', error => {
+    console.error('Uh oh! Database error!!!!')
+    console.error(error)
+    process.exit(1)
+  })
+  return db
 }
-const db = new sqlite3.Database(dbPath, () => {
-  console.log('created database!')
-  createDatabase()
-})
+
+function getFiles() {
+  const dirFiles = fs.readdirSync(__dirname)
+  const csvs = _.filter(dirFiles, f => _.endsWith(f, '.csv'))
+  return csvs
+}
+
+async function getTableSchema(csvName) {
+  const csvjson = await loadCsv(csvName)
+
+  const columns = _.map(_.keys(csvjson[0]), columnName => {
+    const dataValue = _.find(csvjson, row => !_.isNil(row[columnName]))[columnName]
+    if (isNotANumber(dataValue)) {
+      return `    ${columnName} TEXT`
+    }
+    if (_.isInteger(_.toNumber(dataValue))) {
+      return `    ${columnName} INTEGER`
+    }
+    return `    ${columnName} REAL`
+  })
+  const tableName = csvName.replace('.csv', '')
+  const createTable = `CREATE TABLE ${tableName} (
+${_.join(columns, ',\n')}
+  );`
+  const insert = `INSERT INTO ${tableName} VALUES`
+  return { createTable, insert }
+}
 
 
-
-const createDatabase = () => {
+async function populateDatabase(db, csvs) {
   let insertStatements
 
-  db.on('error', error => {
-    console.log(error)
-    console.log(_.join(insertStatements || [], '\r\n'))
-    console.log('fuck')
-  })
-
-  const allLines = []
   const runStatement = line => {
-    console.log(line.substr(0, 200))
+    console.log(line.substr(0, 100))
     db.exec(line)
-    allLines.push(line)
   }
 
+  await Bluebird.map(csvs, async csvpath => {
+    const schema = await getTableSchema(csvpath)
+    runStatement(schema.createTable)
 
-  Bluebird.map(_.take(_.keys(csvs), 10), csvpath => {
-    const scripts = csvs[csvpath]
-    runStatement(scripts[0])
-
-    return csv().fromFile(path.join(__dirname, csvpath)).then(json => {
-      const chunks = _.chunk(json, 10000)
-      _.each(chunks, rows => {
-        insertStatements = [scripts[1]]
-        _.each(rows, row => {
-          const values = _.map(_.values(row), val => {
-            if (val === '') {
-              return 'NULL'
+    const csvjson = await loadCsv(csvpath)
+    const chunks = _.chunk(csvjson, 10000)
+    _.each(chunks, rows => {
+      insertStatements = [schema.insert]
+      _.each(rows, row => {
+        const values = _.map(_.values(row), val => {
+          if (val === '') {
+            return 'NULL'
+          }
+          if (isNotANumber(val)) {
+            var split = _.split(val, '/')
+            if (split.length === 3) {
+              return `'${split[2]}-${split[0].length === 1 ? "0" + split[0] : split[0]}-${split[1].length === 1 ? "0" + split[1] : split[1]}'`
             }
-            if (_.isNaN(_.toNumber(val))) { // Not a number
-              var split = _.split(val, '/')
-              if (split.length === 3) {
-                return `'${split[2]}-${split[0].length === 1 ? "0" + split[0] : split[0]}-${split[1].length === 1 ? "0" + split[1] : split[1]}'`
-              }
 
-              return `'${val.replace("'", "''")}'`
-            }
-            return val
-          })
-          insertStatements.push(`(${_.join(values, ', ')}),`)
+            return `'${val.replace("'", "''")}'`
+          }
+          return val
         })
-        const l = insertStatements.length
-        insertStatements[l - 1] = insertStatements[l - 1].replace(/.$/, ';')
-        const sqlToRun = _.join(insertStatements, '\r\n')
-        runStatement(sqlToRun)
+        insertStatements.push(`(${_.join(values, ', ')}),`)
       })
-
-      const filePath = path.join(__dirname, csvpath.replace('.csv', '.sql'))
-      if (fs.existsSync(filePath)) {
-        fs.unlinkSync(filePath)
-      }
-      fs.writeFileSync(filePath, _.join(allLines, '\r\n'))
-
+      const l = insertStatements.length
+      insertStatements[l - 1] = insertStatements[l - 1].replace(/.$/, ';')
+      const sqlToRun = _.join(insertStatements, '\n')
+      runStatement(sqlToRun)
     })
-  }).then(() => {
-    db.close()
-    console.log('done!')
   })
 }
+
+run()
